916ecec8bd4ba3a8d1e38e0d96a24999b84e71fe
Nick Powell <nickpowells@gmail.com>
2017-07-14 15:02:08 -0700

    *last commit only update* add delete method and flash messages for crud.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -41,6 +41,13 @@ class ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    flash[:alert] = "Product deleted!"
+    redirect_to products_path
+  end
+
   private
   def product_params
     params.require(:product).permit(:name, :description, :price)
