96d3814e067fef2beee5739211981ac85ba8ad37
Nick Powell <nickpowells@gmail.com>
2017-07-14 10:29:43 -0700

    add order items view for js

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -5,7 +5,10 @@ class OrderItemsController < ApplicationController
     @item = @order.order_items.new(item_params)
     @order.save
     session[:order_id] = @order.id
-    redirect_to products_path
+    respond_to do |format|
+      format.html { redirect_to tasks_url }
+      format.js
+    end
   end
 
   def update
