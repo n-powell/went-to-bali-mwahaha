fbb205e2853b0b8b6cd43c02cfa6075b354a5dc7
Nick Powell <nickpowells@gmail.com>
2017-07-14 12:01:12 -0700

    add validation for positive order quantity

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,4 +2,5 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
   validates :quantity, :presence => true
+  validates :quantity, :numericality => { :greater_than_or_equal_to => 0 }
 end
